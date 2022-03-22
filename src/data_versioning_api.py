9b631c6b4e7b4813b3fd4d9eb416b2ff3cc6da28
Noah Marvisi <92672201+nehma158@users.noreply.github.com>
2022-03-22 15:21:46 +0100

    elkrgjelgjergj

---
+++ src/data_versioning_api.py
@@ -0,0 +1,58 @@
+# Get url from DVC
+import sys
+import os
+import getopt
+from pathlib import Path
+import pandas as pd
+import dvc.api
+
+#path='data/wine_original.csv'
+#repo=r'C:\Users\noah-\Documents\01-DSTI\MlOps\env_mlops'
+
+'''
+path = Path(sys.argv[1])
+if len(sys.argv)==2:
+    repo = os.getcwd()
+else:
+    repo = Path(sys.argv[2])
+
+version = 'v2'
+'''
+
+
+# default
+repo = os.getcwd()
+path = r'data/wine_original.csv'
+version = 'HEAD'
+
+argv = sys.argv[1:]
+
+# get arguments in command-line with 
+try:
+    options, args = getopt.getopt(argv, "p:r:v:",
+                               ["path =",
+                                "repo =",
+                                "version="])
+except:
+    print("Error Message ")
+ 
+for name, value in options:
+    if name in ['-p', '--path']:
+        path = value
+    elif name in ['-r', '--repo']:
+        repo = value
+    elif name in ['-v', '--version']:
+        version = value
+
+# Open the csv
+with dvc.api.open(repo=repo, path=path, rev=version, mode="r") as fd:
+    df = pd.read_csv(fd, sep=";")
+    
+    print('')
+    print('repository: ', repo)
+    print('data path: ', path)
+    print('version :', version)
+    print('')
+    print('url :', dvc.api.get_url(repo=repo, path=path))
+    print('')
+    print(df)
